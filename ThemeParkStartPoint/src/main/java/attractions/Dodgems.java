926fc3804b64ac14775f5a3eaf36a965f88a890d
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-02 19:52:35 +0000

    completed MVP

--- ThemeParkStartPoint/src/main/java/attractions/Dodgems.java
+++ ThemeParkStartPoint/src/main/java/attractions/Dodgems.java
@@ -1,8 +1,29 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements ITicketed {
+
+    private double startingPrice;
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+
+        startingPrice = 4.50;
+    }
+
+    @Override
+    public double defaultPrice() {
+        return startingPrice;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        double price = startingPrice;
+        if (visitor.getAge() < 12)
+            price /= 2;
+
+        return price;
     }
 }
--- ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
@@ -2,6 +2,7 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,5 +29,22 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canGetDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.0);
+    }
+
+    @Test
+    public void canGetPriceFor_default() {
+        Visitor visitor = new Visitor(12, 144, 20);
+        assertEquals(4.50, dodgems.priceFor(visitor), 0.0);
+    }
+
+    @Test
+    public void canGetPriceFor_YoungPerson() {
+        Visitor visitor = new Visitor(11, 201, 20);
+        assertEquals(2.25, dodgems.priceFor(visitor), 0.0);
     }
 }
